35886645ee585d9f23cf91b6d2e314e124541075
Minea Lehtinen <minea.v.lehtinen@gmail.com>
2023-05-24 17:20:09 +0300

    Add Repositories to services and database initialisation
    Ensures database migration and adds some dummy data

--- bike-webapi/Program.cs
+++ bike-webapi/Program.cs
@@ -1,11 +1,16 @@
-using bike_webapi.Data;
 using Microsoft.EntityFrameworkCore;
+using bike_webapi.Data;
+using bike_webapi.Interfaces;
+using bike_webapi.Repositories;
+using bike_webapi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IStationRepository, StationRepository>();
+builder.Services.AddScoped<IJourneyRepository, JourneyRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,5 +31,78 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    context.Database.Migrate();
+
+    if (!context.Stations.Any())
+    {
+        context.Stations.AddRange(
+            new List<Station>()
+            {
+                new Station() 
+                { 
+                    Nimi = "Jenkruutin asema",
+                    Namn = "Jöngruds station",
+                    Name = "Jonssons station",
+                    Osoite = "Blabla 1",
+                    Adress = "Blabla 1",
+                    Kaupunki = "Tsadi",
+                    Stad = "Tsad",
+                    Operaattori = "Veikko Kilkenblad Oy",
+                    Kapasiteetti = 5,
+                    X = 99.4,
+                    Y = 99.9
+                },
+                new Station()
+                { 
+                    Nimi = "Kahvila asema",
+                    Namn = "kaffe station",
+                    Name = "Cafe station",
+                    Osoite = "Önsrötinkatu 1",
+                    Adress = "Önsrötsgatan 1",
+                    Kaupunki = "Epsoo",
+                    Stad = "Epsoo",
+                    Operaattori = "Veikko Kilkenblad Oy",
+                    Kapasiteetti = 5,
+                    X = 99.4,
+                    Y = 99.9
+                }
+            }
+        );
+        context.SaveChanges();
+    }
+    if (!context.Journeys.Any())
+    {
+        context.Journeys.AddRange(
+            new List<Journey>()
+            {
+                new Journey()
+                {
+                    Departure = new DateTime(),
+                    Return = new DateTime(),
+                    DepartureStationId = 1,
+                    ReturnStationId = 2,
+                    CoveredDistance = 8393,
+                    Duration = 300
+                },
+                new Journey()
+                {
+                    Departure = new DateTime(),
+                    Return = new DateTime(),
+                    DepartureStationId = 2,
+                    ReturnStationId = 1,
+                    CoveredDistance = 3835,
+                    Duration = 120
+                }
+            }
+        );
+        context.SaveChanges();
+    }
+    
+}
 
 app.Run();
