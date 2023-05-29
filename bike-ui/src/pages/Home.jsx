aa1c099d1b55cd98d0b00c6cd9d0b5511718c1b5
Minea Lehtinen <minea.v.lehtinen@gmail.com>
2023-05-29 13:35:09 +0300

    Add some introduction text to the home page

--- bike-ui/src/pages/Home.jsx
+++ bike-ui/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import { Typography } from "@mui/material";
 
 function Home() {
-  return <Typography>Home</Typography>;
+  return (
+    <>
+      <Typography variant="h4" sx={{ mb: 5, mt: 5 }}>
+        Welcome to Helsinki City Bike App
+      </Typography>
+      <Typography>
+        This is an app made for Solita Dev Academy excercise. It's a fun little
+        app that displays bike journeys and lists stations using open data from
+        HSL. Go check the listings and have fun!
+      </Typography>
+    </>
+  );
 }
 
 export default Home;
